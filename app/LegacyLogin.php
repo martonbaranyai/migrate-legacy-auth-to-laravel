5d614a0a312327cb7634538dc1ae79e87f55dd58
martonbaranyai <baranyaimarci92@gmail.com>
2016-07-28 13:57:09 +0200

    LegacyLogin class should extend the Authenticable

--- app/LegacyLogin.php
+++ app/LegacyLogin.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class LegacyLogin extends Model
+class LegacyLogin extends Authenticatable
 {
     //
 }
