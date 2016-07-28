620899a53c2f68e08abcb3d4c357dd26a6abe7c5
martonbaranyai <baranyaimarci92@gmail.com>
2016-07-28 13:14:37 +0200

    make:auth and a test to make sure it keeps working

---
+++ tests/AuthTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $user = factory(User::class)->create([
+            'name'     => 'John Smith',
+            'email'    => 'john@example.com',
+            'password' => bcrypt('asdf1234'),
+        ]);
+
+        $this->post('/login', [
+            'email'    => 'john@example.com',
+            'password' => 'asdf1234',
+        ])->followRedirects()
+            ->see('Welcome')
+            ->see('John Smith');
+    }
+}
