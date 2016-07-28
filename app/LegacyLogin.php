9831d22eb152b22deb87fc3a84b6f0c77a09901e
martonbaranyai <baranyaimarci92@gmail.com>
2016-07-28 14:16:10 +0200

    be more explicit about the table name

--- app/LegacyLogin.php
+++ app/LegacyLogin.php
@@ -6,5 +6,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class LegacyLogin extends Authenticatable
 {
-    //
+    /**
+     * Table name where the legacy user accounts can be found
+     *
+     * @var string
+     */
+    protected $table = 'legacy_logins';
 }
